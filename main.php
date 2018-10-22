83a58eaa32565e9f9b644a48103d58925f1a9273
Marat Petrosov <marat.petrosov@gmail.com>
2018-10-22 22:57:41 +0200

    first layout

---
+++ main.php
@@ -0,0 +1,79 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- normalisation of styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+    <!-- own styles -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Handlee|Source+Code+Pro" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+    <title>myshop</title>
+</head>
+<body>
+    <div class="container">
+        <div class="nav"><a href="main.php" class="name"> My Litle Shop.com </a>
+            
+            <div class="help">
+                <a href="help.php" class="button-help">  help <i class="fas fa-info-circle"></i></a>
+            </div>
+            <div class="message">
+            <a href="messages.php" class="button-help">messages<i class="fas fa-comments"></i></a>
+            </div>
+            <div class="login">
+            <a href="login.php" class="button-help">login<i class="fas fa-sign-in-alt"></i></a>
+            </div>
+        </div>
+        <div class="search-box">
+
+        </div>
+        <div class="sidebar">sidebar
+            <div class="category">
+                category 1 
+            </div>
+            <div class="category">
+                category 2 
+            </div>
+            <div class="category">
+                category 3 
+            </div>
+            <div class="category">
+                category 4 
+            </div>
+            <div class="category">
+                category 5 
+            </div>
+            <div class="category">
+                category 6 
+            </div>
+        </div>
+        <div class="content">Content
+            <div class="product-view">
+                product 1 view
+            </div>
+            <div class="product-view">
+                product 2 view
+            </div>
+            <div class="product-view">
+                product3
+            </div>
+            <div class="product-view">
+                product 4 view 
+            </div>
+            <div class="product-view">
+                product 5 view
+            </div>
+        </div>
+        <div class="footer">Footer
+        </div>        
+    </div>
+</body>
+</html>
